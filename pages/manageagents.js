3fbf8b4a01f5587d6433af398023d36f67e79206
hunfa <nabeelmuzaffar0323@hotmail.com>
2022-11-18 16:22:28 +0500

    update on manageagents

--- pages/manageagents.js
+++ pages/manageagents.js
@@ -16,15 +16,15 @@ function Manageagents() {
    <>
    <Box mx="10px" mb="30px">
    <TableContainer maxWidth={"1500px"} mx="auto" border={"1px"} borderColor="gray.400" borderRadius={"8px"} mt="50px">
-  <Table variant='simple' colorScheme='teal'>
+  <Table variant='striped' colorScheme="blue"   >
    
     <TableCaption mt="0px" fontSize={"2rem"} placement={"top"}>Manage Agents</TableCaption>
     <Thead>
     
       <Tr>
         <Th>Username</Th>
-        <Th>Password</Th>
-        <Th isNumeric>Actions</Th>
+        <Th >Password</Th>
+        <Th pr="50px" isNumeric >Actions</Th>
       </Tr>
     </Thead>
     <Tbody>
@@ -32,25 +32,46 @@ function Manageagents() {
         <Td > 
         <Text>  Nabeel Tours And Travels</Text></Td>
         <Td>nabeel123 </Td>
-        <Td isNumeric><Button size={"sm"} colorScheme={"red"}> Delete</Button></Td>
+        <Td isNumeric>
+        <Button mr="6px" size={"sm"} colorScheme={"yellow"}> Edit</Button>
+          <Button size={"sm"} colorScheme={"red"}> Delete</Button>
+       
+        </Td>
       </Tr>
       <Tr>
-        <Td>feet</Td>
-        <Td>centimetres (cm)</Td>
-        <Td isNumeric>30.48</Td>
+        <Td > 
+        <Text>  Nabeel Tours And Travels</Text></Td>
+        <Td>nabeel123 </Td>
+        <Td isNumeric>
+        <Button mr="6px" size={"sm"} colorScheme={"yellow"}> Edit</Button>
+          <Button size={"sm"} colorScheme={"red"}> Delete</Button>
+       
+        </Td>
       </Tr>
       <Tr>
-        <Td>yards</Td>
-        <Td>metres (m)</Td>
-        <Td isNumeric>0.91444</Td>
+        <Td > 
+        <Text>  Nabeel Tours And Travels</Text></Td>
+        <Td>nabeel123 </Td>
+        <Td isNumeric>
+        <Button mr="6px" size={"sm"} colorScheme={"yellow"}> Edit</Button>
+          <Button size={"sm"} colorScheme={"red"}> Delete</Button>
+       
+        </Td>
       </Tr>
+      <Tr>
+        <Td > 
+        <Text>  Nabeel Tours And Travels</Text></Td>
+        <Td>nabeel123 </Td>
+        <Td isNumeric>
+        <Button mr="6px" size={"sm"} colorScheme={"yellow"}> Edit</Button>
+          <Button size={"sm"} colorScheme={"red"}> Delete</Button>
+       
+        </Td>
+      </Tr>
+     
     </Tbody>
     <Tfoot>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
+     
     </Tfoot>
   </Table>
 </TableContainer>
